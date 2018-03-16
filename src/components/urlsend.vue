d8f88c8f9cff477d5927a2829c4d9e27193940ea
kain <826589265@qq.com>
2018-03-16 17:18:11 +0800

    Signed-off-by: kain <826589265@qq.com>

--- src/components/urlsend.vue
+++ src/components/urlsend.vue
@@ -2,7 +2,10 @@
   <div class="urlsend">
     <input v-model='url'/>
     <button type='button' v-on:click='click'>go</button>
-    <img v-for="item in items" v-bind:src="item.src" alt=""></img>
+    <button type='button' v-on:click='click3'>upload</button>
+    <div v-for="item in items" class='nomask'>
+    <img v-bind:src="item.src" alt="" v-on:click='click2'></img>
+    </div>
   </div>
 </template>
 
@@ -13,7 +16,8 @@ export default {
   data () {
     return {
       url: '',
-      items: []
+      items: [],
+      select: []
     }
   },
   methods: {
@@ -35,6 +39,39 @@ export default {
         }).then(function (json) {
           console.log('parsed json', json)
           that.items = json
+        }).catch(function (ex) {
+          console.log('parsing failed', ex)
+        })
+    },
+    click2: function (event) {
+      if (this.select.indexOf(event.target.src) === -1) {
+        this.select.push(event.target.src)
+        event.target.parentNode.className = 'mask'
+      } else {
+        this.select.splice(this.select.indexOf(event.target.src), 1)
+        event.target.parentNode.className = 'nomask'
+      }
+      console.log(this.select)
+    },
+    click3: function (event) {
+      var that = this
+      fetch('http://47.94.196.19:3000/api/spdpic', {
+        method: 'POST',
+        body: JSON.stringify({
+          thumb: that.select[0],
+          albumname: '新建图集',
+          piclist: that.select,
+          num: that.select.length
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(function (response) {
+          return response.json()
+        }).then(function (json) {
+          console.log('parsed json', json)
+          alert('添加成功')
         }).catch(function (ex) {
           console.log('parsing failed', ex)
         })
@@ -61,5 +98,15 @@ li {
 
 a {
   color: #42b983;
+}
+
+.mask{
+  opacity:0.4;
+  filter: brightness(0.5);
+  float: left;
+}
+
+.nomask{
+  float: left;
 }
 </style>
